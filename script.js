d79ed945ed682a515ac7994daa81767f661da66c
Aarav Juneja (He/Him) <aarav.juneja@ais.amity.edu>
2023-06-05 14:03:53 +0530

    Twentieth Commit

--- script.js
+++ script.js
@@ -43,6 +43,7 @@ loginForm.addEventListener("submit", function (e) {
 
   if (username && password) {
     loggedIn = true;
+    window.location.href = 'console.html';
     loginForm.style.display = "none";
     consoleSection.style.display = "block";
     updateDashboard("HCET Syndicate TARDIS Console", "Unknown", "Unknown");
