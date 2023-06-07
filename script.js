769d344fb1e42e641c5454e7e505e4973a42abcb
Aarav Juneja (He/Him) <aarav.juneja@ais.amity.edu>
2023-06-07 22:49:08 +0530

    Improved Login Section

--- script.js
+++ script.js
@@ -1,17 +1,68 @@
-// DOM elements
-const loginForm = document.getElementById("login-form");
+document.addEventListener('DOMContentLoaded', function() {
+  const signupForm = document.getElementById('signup-form');
+  const loginForm = document.getElementById('login-form');
+  const signupContainer = document.getElementById('signup-container');
+  const loginContainer = document.getElementById('login-container');
+  const showSignupLink = document.getElementById('show-signup-link');
 
-// Variables
-let loggedIn = false;
+  signupContainer.style.display = 'none';
 
-// Event Listener
-loginForm.addEventListener("submit", function (e) {
-  e.preventDefault();
-  const username = document.getElementById("username").value;
-  const password = document.getElementById("password").value;
+  showSignupLink.addEventListener('click', function (event) {
+    event.preventDefault();
+    showSignupForm();
+  });
 
-  if (username && password) {
-    loggedIn = true;
-    window.location.href = 'console.html';
+  signupForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const signupUsername = document.getElementById('signup-username').value;
+    const signupPassword = document.getElementById('signup-password').value;
+    const signupEmail = document.getElementById('signup-email').value;
+
+    signupForm.reset();
+
+    localStorage.setItem('username', signupUsername);
+    localStorage.setItem('password', signupPassword);
+    localStorage.setItem('email', signupEmail);
+
+    alert('Sign up successful! Please log in.');
+
+    showLoginForm();
+    scrollToLoginForm();
+  });
+
+  loginForm.addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    const enteredUsername = document.getElementById('username').value;
+    const enteredPassword = document.getElementById('password').value;
+
+    const storedUsername = localStorage.getItem('username');
+    const storedPassword = localStorage.getItem('password');
+
+    if (enteredUsername === storedUsername && enteredPassword === storedPassword) {
+      loginForm.reset();
+      window.location.href = 'console.html';
+    } else {
+      alert('Invalid username or password. Please try again.');
+    }
+  });
+
+  function showLoginForm() {
+    signupContainer.style.display = 'none';
+    loginContainer.style.display = 'block';
+  }
+
+  function showSignupForm() {
+    loginContainer.style.display = 'none';
+    signupContainer.style.display = 'block';
+  }
+
+  function scrollToLoginForm() {
+    const loginContainerTop = loginContainer.getBoundingClientRect().top;
+    window.scrollTo({
+      top: loginContainerTop,
+      behavior: 'smooth'
+    });
   }
 });
